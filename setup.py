f816c83436deb2d6929c5b38889df2ad2d518cab
Chris Withers <cwithers@batstrading.com>
2012-04-13 17:01:12 +0100

    First pass at Sphinx docs.

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-# Copyright (c) 2008-2010 Simplistix Ltd
-#
-# This Software is released under the MIT License:
-# http://www.opensource.org/licenses/mit-license.html
-# See license.txt for more details.
+# Copyright (c) 2008-2012 Simplistix Ltd
+# See license.txt for license details.
 
 import os
 from setuptools import setup
+
+name = 'xlutils'
+base_dir = os.path.dirname(__file__)
 
 package_dir = os.path.join(os.path.dirname(__file__),'xlutils')
 
 setup(
     name='xlutils',
-    version=file(os.path.join(package_dir,'version.txt')).read().strip(),
+    version=file(os.path.join(base_dir, name, 'version.txt')).read().strip(),
     author='Chris Withers',
     author_email='chris@simplistix.co.uk',
     license='MIT',
     description="Utilities for working with Excel files that require both xlrd and xlwt",
-    long_description=open(os.path.join(package_dir,'docs','description.txt')).read(),
+    long_description=open(os.path.join(base_dir, 'docs', 'description.txt')).read(),
     url='http://www.simplistix.co.uk/software/python/xlutils',
     keywords="excel xls xlrd xlwt",
     classifiers=[
@@ -45,8 +45,3 @@ setup(
             ],
            )
     )
-
-#  bin/py setup.py sdist register upload
-
-# To check how things will show on pypi, install docutils and then:
-# bin/buildout -q setup setup.py --long-description | rst2html.py --link-stylesheet --stylesheet=http://www.python.org/styles/styles.css > dist/desc.html
